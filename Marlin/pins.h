ec4933ff649eee3c4a32ffbd50c38e2aac0c1fed
marco antonini <marcoantonini@marcomac-eth.lan>
2013-01-30 18:50:24 +0100

    Added pinout Ramp support (Motherboard 3,33,34)

--- Marlin/pins.h
+++ Marlin/pins.h
@@ -152,11 +152,6 @@
 #endif
 
 
-
-#ifndef KNOWN_BOARD
-#error Unknown MOTHERBOARD value in configuration.h
-#endif
-
 //List of pins which to ignore when asked to change by gcode, 0 and 1 are RX and TX, do not mess with those!
 #define _E0_PINS E0_STEP_PIN, E0_DIR_PIN, E0_ENABLE_PIN, HEATER_0_PIN,
 #ifdef REPRAPPRO_MULTIMATERIALS
@@ -185,5 +180,458 @@
                         HEATER_BED_PIN, FAN_PIN,                  \
                         _E0_PINS _E1_PINS _E2_PINS             \
                         TEMP_0_PIN, TEMP_1_PIN, TEMP_2_PIN, TEMP_BED_PIN }
+
+
+
+/****************************************************************************************
+ * Arduino Mega pin assignment
+ *
+ ****************************************************************************************/
+#if MOTHERBOARD == 3 || MOTHERBOARD == 33 || MOTHERBOARD == 34
+#define KNOWN_BOARD 1
+
+//////////////////FIX THIS//////////////
+#ifndef __AVR_ATmega1280__
+#ifndef __AVR_ATmega2560__
+#error Oops!  Make sure you have 'Arduino Mega' selected from the 'Tools -> Boards' menu.
+#endif
+#endif
+
+// uncomment one of the following lines for RAMPS v1.3 or v1.0, comment both for v1.2 or 1.1
+// #define RAMPS_V_1_3
+// #define RAMPS_V_1_0
+
+#if MOTHERBOARD == 33 || MOTHERBOARD == 34
+
+#define X_STEP_PIN         54
+#define X_DIR_PIN          55
+#define X_ENABLE_PIN       38
+#define X_MIN_PIN           3
+#define X_MAX_PIN           2
+
+#define Y_STEP_PIN         60
+#define Y_DIR_PIN          61
+#define Y_ENABLE_PIN       56
+#define Y_MIN_PIN          14
+#define Y_MAX_PIN          15
+
+#define Z_STEP_PIN         46
+#define Z_DIR_PIN          48
+#define Z_ENABLE_PIN       62
+#define Z_MIN_PIN          18
+#define Z_MAX_PIN          19
+
+#define Z2_STEP_PIN        36
+#define Z2_DIR_PIN         34
+#define Z2_ENABLE_PIN      30
+
+#define E0_STEP_PIN        26
+#define E0_DIR_PIN         28
+#define E0_ENABLE_PIN      24
+
+#define E1_STEP_PIN        36
+#define E1_DIR_PIN         34
+#define E1_ENABLE_PIN      30
+
+#define SDPOWER            -1
+#define SDSS               53
+#define LED_PIN            13
+
+#if MOTHERBOARD == 33
+#define FAN_PIN            9 // (Sprinter config)
+#else
+#define FAN_PIN            4 // IO pin. Buffer needed
+#endif
+#define PS_ON_PIN          12
+
+#if defined(REPRAP_DISCOUNT_SMART_CONTROLLER) || defined(G3D_PANEL)
+#define KILL_PIN           41
+#else
+#define KILL_PIN           -1
+#endif
+
+#define HEATER_0_PIN       10   // EXTRUDER 1
+#if MOTHERBOARD == 33
+#define HEATER_1_PIN       -1
+#else
+#define HEATER_1_PIN       9    // EXTRUDER 2 (FAN On Sprinter)
+#endif
+#define HEATER_2_PIN       -1
+#define TEMP_0_PIN         13   // ANALOG NUMBERING
+#define TEMP_1_PIN         15   // ANALOG NUMBERING
+#define TEMP_2_PIN         -1   // ANALOG NUMBERING
+#define HEATER_BED_PIN     8    // BED
+#define TEMP_BED_PIN       14   // ANALOG NUMBERING
+
+#ifdef ULTRA_LCD
+
+#ifdef NEWPANEL
+//encoder rotation values
+#define encrot0 0
+#define encrot1 2
+#define encrot2 3
+#define encrot3 1
+
+#define BLEN_A 0
+#define BLEN_B 1
+#define BLEN_C 2
+
+#define LCD_PINS_RS 16
+#define LCD_PINS_ENABLE 17
+#define LCD_PINS_D4 23
+#define LCD_PINS_D5 25
+#define LCD_PINS_D6 27
+#define LCD_PINS_D7 29
+
+#ifdef REPRAP_DISCOUNT_SMART_CONTROLLER
+#define BEEPER 37
+
+#define BTN_EN1 31
+#define BTN_EN2 33
+#define BTN_ENC 35
+
+#define SDCARDDETECT 49
+#else
+//arduino pin which triggers an piezzo beeper
+#define BEEPER 33	 // Beeper on AUX-4
+
+//buttons are directly attached using AUX-2
+#define BTN_EN1 37
+#define BTN_EN2 35
+#define BTN_ENC 31  //the click
+
+#ifdef G3D_PANEL
+#define SDCARDDETECT 49
+#else
+#define SDCARDDETECT -1  // Ramps does not use this port
+#endif
+#endif
+
+#else //old style panel with shift register
+//arduino pin witch triggers an piezzo beeper
+#define BEEPER 33		No Beeper added
+
+//buttons are attached to a shift register
+// Not wired this yet
+//#define SHIFT_CLK 38
+//#define SHIFT_LD 42
+//#define SHIFT_OUT 40
+//#define SHIFT_EN 17
+
+#define LCD_PINS_RS 16
+#define LCD_PINS_ENABLE 17
+#define LCD_PINS_D4 23
+#define LCD_PINS_D5 25
+#define LCD_PINS_D6 27
+#define LCD_PINS_D7 29
+
+//encoder rotation values
+#define encrot0 0
+#define encrot1 2
+#define encrot2 3
+#define encrot3 1
+
+
+//bits in the shift register that carry the buttons for:
+// left up center down right red
+#define BL_LE 7
+#define BL_UP 6
+#define BL_MI 5
+#define BL_DW 4
+#define BL_RI 3
+#define BL_ST 2
+
+#define BLEN_B 1
+#define BLEN_A 0
+#endif
+#endif //ULTRA_LCD
+
+#else // RAMPS_V_1_1 or RAMPS_V_1_2 as default (MOTHERBOARD == 3)
+
+#define X_STEP_PIN         26
+#define X_DIR_PIN          28
+#define X_ENABLE_PIN       24
+#define X_MIN_PIN           3
+#define X_MAX_PIN          -1    //2
+
+#define Y_STEP_PIN         38
+#define Y_DIR_PIN          40
+#define Y_ENABLE_PIN       36
+#define Y_MIN_PIN          16
+#define Y_MAX_PIN          -1    //17
+
+#define Z_STEP_PIN         44
+#define Z_DIR_PIN          46
+#define Z_ENABLE_PIN       42
+#define Z_MIN_PIN          18
+#define Z_MAX_PIN          -1    //19
+
+#define E0_STEP_PIN         32
+#define E0_DIR_PIN          34
+#define E0_ENABLE_PIN       30
+
+#define SDPOWER            48
+#define SDSS               53
+#define LED_PIN            13
+#define PS_ON_PIN          -1
+#define KILL_PIN           -1
+
+#ifdef RAMPS_V_1_0 // RAMPS_V_1_0
+#define HEATER_0_PIN     12    // RAMPS 1.0
+#define HEATER_BED_PIN   -1    // RAMPS 1.0
+#define FAN_PIN          11    // RAMPS 1.0
+#else // RAMPS_V_1_1 or RAMPS_V_1_2
+#define HEATER_0_PIN     10    // RAMPS 1.1
+#define HEATER_BED_PIN    8    // RAMPS 1.1
+#define FAN_PIN           9    // RAMPS 1.1
+#endif
+#define HEATER_1_PIN        -1
+#define HEATER_2_PIN        -1
+#define TEMP_0_PIN          2    // MUST USE ANALOG INPUT NUMBERING NOT DIGITAL OUTPUT NUMBERING!!!!!!!!!
+#define TEMP_1_PIN          -1
+#define TEMP_2_PIN          -1
+#define TEMP_BED_PIN        1    // MUST USE ANALOG INPUT NUMBERING NOT DIGITAL OUTPUT NUMBERING!!!!!!!!!
+#endif// MOTHERBOARD == 33 || MOTHERBOARD == 34
+
+// SPI for Max6675 Thermocouple
+
+#ifndef SDSUPPORT
+// these pins are defined in the SD library if building with SD support
+#define MAX_SCK_PIN          52
+#define MAX_MISO_PIN         50
+#define MAX_MOSI_PIN         51
+#define MAX6675_SS       53
+#else
+#define MAX6675_SS       49
+#endif
+
+#endif//MOTHERBOARD == 3 || MOTHERBOARD == 33 || MOTHERBOARD == 34
+
+/****************************************************************************************
+ * Arduino Mega pin assignment
+ *
+ ****************************************************************************************/
+#if MOTHERBOARD == 3 || MOTHERBOARD == 33 || MOTHERBOARD == 34
+#define KNOWN_BOARD 1
+
+//////////////////FIX THIS//////////////
+#ifndef __AVR_ATmega1280__
+#ifndef __AVR_ATmega2560__
+#error Oops!  Make sure you have 'Arduino Mega' selected from the 'Tools -> Boards' menu.
+#endif
+#endif
+
+// uncomment one of the following lines for RAMPS v1.3 or v1.0, comment both for v1.2 or 1.1
+// #define RAMPS_V_1_3
+// #define RAMPS_V_1_0
+
+#if MOTHERBOARD == 33 || MOTHERBOARD == 34
+
+#define X_STEP_PIN         54
+#define X_DIR_PIN          55
+#define X_ENABLE_PIN       38
+#define X_MIN_PIN           3
+#define X_MAX_PIN           2
+
+#define Y_STEP_PIN         60
+#define Y_DIR_PIN          61
+#define Y_ENABLE_PIN       56
+#define Y_MIN_PIN          14
+#define Y_MAX_PIN          15
+
+#define Z_STEP_PIN         46
+#define Z_DIR_PIN          48
+#define Z_ENABLE_PIN       62
+#define Z_MIN_PIN          18
+#define Z_MAX_PIN          19
+
+#define Z2_STEP_PIN        36
+#define Z2_DIR_PIN         34
+#define Z2_ENABLE_PIN      30
+
+#define E0_STEP_PIN        26
+#define E0_DIR_PIN         28
+#define E0_ENABLE_PIN      24
+
+#define E1_STEP_PIN        36
+#define E1_DIR_PIN         34
+#define E1_ENABLE_PIN      30
+
+#define SDPOWER            -1
+#define SDSS               53
+#define LED_PIN            13
+
+#if MOTHERBOARD == 33
+#define FAN_PIN            9 // (Sprinter config)
+#else
+#define FAN_PIN            4 // IO pin. Buffer needed
+#endif
+#define PS_ON_PIN          12
+
+#if defined(REPRAP_DISCOUNT_SMART_CONTROLLER) || defined(G3D_PANEL)
+#define KILL_PIN           41
+#else
+#define KILL_PIN           -1
+#endif
+
+#define HEATER_0_PIN       10   // EXTRUDER 1
+#if MOTHERBOARD == 33
+#define HEATER_1_PIN       -1
+#else
+#define HEATER_1_PIN       9    // EXTRUDER 2 (FAN On Sprinter)
+#endif
+#define HEATER_2_PIN       -1
+#define TEMP_0_PIN         13   // ANALOG NUMBERING
+#define TEMP_1_PIN         15   // ANALOG NUMBERING
+#define TEMP_2_PIN         -1   // ANALOG NUMBERING
+#define HEATER_BED_PIN     8    // BED
+#define TEMP_BED_PIN       14   // ANALOG NUMBERING
+
+#ifdef ULTRA_LCD
+
+#ifdef NEWPANEL
+//encoder rotation values
+#define encrot0 0
+#define encrot1 2
+#define encrot2 3
+#define encrot3 1
+
+#define BLEN_A 0
+#define BLEN_B 1
+#define BLEN_C 2
+
+#define LCD_PINS_RS 16
+#define LCD_PINS_ENABLE 17
+#define LCD_PINS_D4 23
+#define LCD_PINS_D5 25
+#define LCD_PINS_D6 27
+#define LCD_PINS_D7 29
+
+#ifdef REPRAP_DISCOUNT_SMART_CONTROLLER
+#define BEEPER 37
+
+#define BTN_EN1 31
+#define BTN_EN2 33
+#define BTN_ENC 35
+
+#define SDCARDDETECT 49
+#else
+//arduino pin which triggers an piezzo beeper
+#define BEEPER 33	 // Beeper on AUX-4
+
+//buttons are directly attached using AUX-2
+#define BTN_EN1 37
+#define BTN_EN2 35
+#define BTN_ENC 31  //the click
+
+#ifdef G3D_PANEL
+#define SDCARDDETECT 49
+#else
+#define SDCARDDETECT -1  // Ramps does not use this port
+#endif
+#endif
+
+#else //old style panel with shift register
+//arduino pin witch triggers an piezzo beeper
+#define BEEPER 33		No Beeper added
+
+//buttons are attached to a shift register
+// Not wired this yet
+//#define SHIFT_CLK 38
+//#define SHIFT_LD 42
+//#define SHIFT_OUT 40
+//#define SHIFT_EN 17
+
+#define LCD_PINS_RS 16
+#define LCD_PINS_ENABLE 17
+#define LCD_PINS_D4 23
+#define LCD_PINS_D5 25
+#define LCD_PINS_D6 27
+#define LCD_PINS_D7 29
+
+//encoder rotation values
+#define encrot0 0
+#define encrot1 2
+#define encrot2 3
+#define encrot3 1
+
+
+//bits in the shift register that carry the buttons for:
+// left up center down right red
+#define BL_LE 7
+#define BL_UP 6
+#define BL_MI 5
+#define BL_DW 4
+#define BL_RI 3
+#define BL_ST 2
+
+#define BLEN_B 1
+#define BLEN_A 0
+#endif
+#endif //ULTRA_LCD
+
+#else // RAMPS_V_1_1 or RAMPS_V_1_2 as default (MOTHERBOARD == 3)
+
+#define X_STEP_PIN         26
+#define X_DIR_PIN          28
+#define X_ENABLE_PIN       24
+#define X_MIN_PIN           3
+#define X_MAX_PIN          -1    //2
+
+#define Y_STEP_PIN         38
+#define Y_DIR_PIN          40
+#define Y_ENABLE_PIN       36
+#define Y_MIN_PIN          16
+#define Y_MAX_PIN          -1    //17
+
+#define Z_STEP_PIN         44
+#define Z_DIR_PIN          46
+#define Z_ENABLE_PIN       42
+#define Z_MIN_PIN          18
+#define Z_MAX_PIN          -1    //19
+
+#define E0_STEP_PIN         32
+#define E0_DIR_PIN          34
+#define E0_ENABLE_PIN       30
+
+#define SDPOWER            48
+#define SDSS               53
+#define LED_PIN            13
+#define PS_ON_PIN          -1
+#define KILL_PIN           -1
+
+#ifdef RAMPS_V_1_0 // RAMPS_V_1_0
+#define HEATER_0_PIN     12    // RAMPS 1.0
+#define HEATER_BED_PIN   -1    // RAMPS 1.0
+#define FAN_PIN          11    // RAMPS 1.0
+#else // RAMPS_V_1_1 or RAMPS_V_1_2
+#define HEATER_0_PIN     10    // RAMPS 1.1
+#define HEATER_BED_PIN    8    // RAMPS 1.1
+#define FAN_PIN           9    // RAMPS 1.1
+#endif
+#define HEATER_1_PIN        -1
+#define HEATER_2_PIN        -1
+#define TEMP_0_PIN          2    // MUST USE ANALOG INPUT NUMBERING NOT DIGITAL OUTPUT NUMBERING!!!!!!!!!
+#define TEMP_1_PIN          -1
+#define TEMP_2_PIN          -1
+#define TEMP_BED_PIN        1    // MUST USE ANALOG INPUT NUMBERING NOT DIGITAL OUTPUT NUMBERING!!!!!!!!!
+#endif// MOTHERBOARD == 33 || MOTHERBOARD == 34
+
+// SPI for Max6675 Thermocouple
+
+#ifndef SDSUPPORT
+// these pins are defined in the SD library if building with SD support
+#define MAX_SCK_PIN          52
+#define MAX_MISO_PIN         50
+#define MAX_MOSI_PIN         51
+#define MAX6675_SS       53
+#else
+#define MAX6675_SS       49
+#endif
+
+#endif//MOTHERBOARD == 3 || MOTHERBOARD == 33 || MOTHERBOARD == 34
+#ifndef KNOWN_BOARD
+#error Unknown MOTHERBOARD value in configuration.h
+#endif
 
 #endif
